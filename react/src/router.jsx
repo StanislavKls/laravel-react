49439fcc09cd1b0edee72f303df9b27345e6ed29
Stanislav Kletsov <klecov@gmail.com>
2023-10-10 21:08:54 +0500

    auth controller, sign up form

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -5,7 +5,7 @@ import Login from "./views/Login";
 import Signup from "./views/Signup";
 import GuestLayout from "./components/GuestLayout";
 import DefaultLayout from "./components/DefaultLayout";
-import SurveyView from "./views/SurveyVies";
+import SurveyView from "./views/SurveyView";
 
 const router = createBrowserRouter([
   {
