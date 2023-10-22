aaaae440ea04eacb3119e7a73a5cee6994f681f4
Stanislav Kletsov <klecov@gmail.com>
2023-10-22 13:58:10 +0500

    login, logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -3,6 +3,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { NavLink, Navigate, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider'
+import axiosClient from '../axios'
 
 
 const navigation = [
@@ -15,15 +16,19 @@ function classNames(...classes) {
 }
 
 export default function DefaultLayout() {
-  const { currentUser, userToken } = useStateContext();
+  const { currentUser, userToken, setCurrentUser, setUserToken } = useStateContext();
 
   if (!userToken) {
     return <Navigate to='login' />
   }
-  
+
   const logout = (ev) => {
     ev.preventDefault();
-    console.log('logout');
+    axiosClient.post('/logout')
+    .then((res) => {
+        setCurrentUser({});
+        setUserToken(null);
+    });
   };
 
   return (
@@ -45,7 +50,7 @@ export default function DefaultLayout() {
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
-                          
+
                           <NavLink
                             key={item.name}
                             to={item.to}
